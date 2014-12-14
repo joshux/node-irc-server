4a94f094b5b21d877605752f9ad87e714322f079
joshux <joshux321@gmail.com>
2014-12-14 11:52:24 +0800

    command arguments and default.conf

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+var util = require('util');
 var net = require('net'); 
-
+var fs = require('fs');
+var events = require('events');
 var Server = function(){
   events.EventEmitter.call(this);
 };
 
-utils.inherit(Server, events.EventEmitter);
+util.inherits(Server, events.EventEmitter);
 
+var nodeID = process.argv[2];
+var configFile = process.argv[3] || 'default.conf';
+
+var buf = fs.readFileSync(configFile);
+console.log(buf.toString());
