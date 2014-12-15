f014f106c14eee853bdb084bcd6955f406ddbc46
joshux <joshux321@gmail.com>
2014-12-15 16:07:36 +0800

    arguments ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var util = require('util');
-var net = require('net'); 
+var net = require('net');
 var fs = require('fs');
 var events = require('events');
 var Server = function(){
@@ -12,4 +12,12 @@ var nodeID = process.argv[2];
 var configFile = process.argv[3] || 'default.conf';
 
 var buf = fs.readFileSync(configFile);
-console.log(buf.toString());
+var conf = JSON.parse(buf.toString());
+
+var nodeIDconf = conf.nodeID;
+if(nodeIDconf !== parseInt(nodeID))
+  throw new Error('error nodeID');
+
+var ircPort = conf['irc-port'];
+
+var clients = [];
